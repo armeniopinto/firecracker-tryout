acb8f7b00815ebcc9a50d2b831d11c6bb0a6f918
Arménio Pinto <armeniopinto@gmail.com>
2022-09-12 11:24:40 +0000

    Improved the output of the server application.

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
+import logging
+logging.getLogger("werkzeug").disabled = True
+
+import flask.cli
+flask.cli.show_server_banner = lambda *args: None
+
 from flask import Flask, request
 
 app = Flask(__name__)
+app.logger.dsabled = True
+
+hellos = ['\N{wavy dash}'] * 32
+#hellos = ['\N{cross mark}'] * 32
 
 @app.route("/hello")
-def flip():
-	print(request.remote_addr)
-	return "", 200
+def hello():
+    vmm_id = int(request.remote_addr.split(".")[2])
+    #hellos[vmm_id] = '\N{grinning face}'
+    hellos[vmm_id] = '\N{fire}'
+    print(''.join(hellos))
+    return "", 200
